3482c0c51746c651fdeddceff4b5066795dd3fce
24LP4621 24LP4621 <24LP4621@G301-CC0195.local>
2024-12-02 16:42:15 +0800

    data types

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello world from JS");
+
+// Data types
+
+// 1. String => Text type => "text"
+// 2. Number => number type => 1 6 100
+// 3. Boolean => true or false => false / true
+// 4. Object => {key:value}
+// 5. Array => [index, index]
+// 6. Null or Undefined => no value
